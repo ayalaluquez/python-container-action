49cd89a1a4cbaef0a3bb8f6d468d3ebf4afc18dc
Blanca Ayala <45976038+ayalaluquez@users.noreply.github.com>
2020-05-05 15:58:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests  # noqa We are just importing this to prove the dependency insta
 def main():
     my_input = os.environ["INPUT_MYINPUT"]
 
-    print(f"::set-output name=myOutput::{my_input}")
+    print(f"::set-output name=myOutput::{myInput}")
 
 
 if __name__ == "__main__":
